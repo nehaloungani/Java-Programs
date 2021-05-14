9e2fa1b934f173173d304a51443aa18534c5aa58
nehaloungani <82209427+nehaloungani@users.noreply.github.com>
2021-05-14 21:29:25 +0500

    Add files via upload

---
+++ Invertedfull.java
@@ -0,0 +1,18 @@
+public class Invertedfull {
+    public static void main(String[] args) {
+        int rows = 5;
+    
+        for(int i = rows; i >= 1; i--) {
+          for(int j = 1; j <= rows - i; j++) {
+            System.out.print("  ");
+        }
+          for(int j=i; j <= 2 * i - 1; j++) {
+            System.out.print("* ");
+        }
+          for(int j = 0; j < i - 1; j++) {
+            System.out.print("* ");
+        }
+          System.out.println();
+        }
+    }  
+}
